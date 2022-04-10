8baac8129b1261314af8d848970b53337b71689c
onggi <hsk1533@naver.com>
2022-04-10 17:54:16 +0900

    middleware description

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
-app.use(morgan("dev"));
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(morgan("dev")); // morgan middlewrare : request and response information show console >> function argument : dev, combined, common, short, tiny
+app.use("/", express.static(path.join(__dirname, "public"))); // static middleware : router == static middelwrare
+
+// body-parser
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.urlencoded({ extended: false })); // extended is false = interpret using quertstring moudule // extended is true = interpret using qs module
+
+app.use(cookieParser(process.env.COOKIE_SECRET)); // make cookies object in interpret cookies
 app.use(
   session({
     resave: false,
@@ -31,9 +34,16 @@ app.use((req, res, next) => {
   console.log("all request");
   next();
 });
-app.get("/", (req, res, next) => {
-  console.log("GET / request");
-});
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("GET / request");
+    next();
+  },
+  (req, res) => {
+    throw new Error("error is gone middleware");
+  }
+);
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send(err.message);
