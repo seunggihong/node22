63c26304c3d449e1715e09c0bb811a491bd735a4
onggi <hsk1533@naver.com>
2022-04-10 17:32:19 +0900

    express middleware

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
 const path = require("path");
 
+dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 3000);
+
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
+
+app.use((req, res, next) => {
+  console.log("all request");
+  next();
+});
+app.get("/", (req, res, next) => {
+  console.log("GET / request");
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
